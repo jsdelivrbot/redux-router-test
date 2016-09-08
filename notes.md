8a63a301170916eb5739f8fe946c2a18d587e677
samnags <sam.nagourney@gmail.com>
2016-09-08 13:59:30 -0400

    added basic routes

---
+++ notes.md
@@ -0,0 +1,12 @@
+Concept questions / not nit picky
+
+
+
+- History: JS package that manages URL of browser. Watches for changes and can update it.
+  - History takes URL and gives it to ReactRouter
+  
+
+- ReactRouter
+  - The RR will change the components on the page based on the URL it's given
+    - Emulating a user navigating a different web page. User thinks they changed pages      
+      entirely.  
--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router'
+import routes from './routes';
 
-import App from './components/app';
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
+
+
+
+// browerHistory is object tells React Router how to interpret changes to URL
+
+// We need to tell React Router what valid routes our app has and which components to show
+  // we'll set them up in routes.js

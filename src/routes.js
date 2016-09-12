bb08b8cdefb4d107723024556352937db573804f
samnags <sam.nagourney@gmail.com>
2016-09-12 19:43:33 -0400

    added index

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,26 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
+const Greeting = () => {
+  return <div>Hey there</div>;
+};
+
+// exporting so routes are importable to index.js
 export default (
-  <Route path="/" component={App}/ >
+  // these are nested routes
+  // child component (greeting) is passed to parent component app
+  <Route path="/" component={App}>
+    // IndexRoute will be default route if path isn't a child
+    <IndexRoute component={PostsIndex} />
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
 );
 
 
 // mathces path of / to App components
+
+// IndexRoute is helper that behaves like a route. It's shown whenever URL matches up with path defiend by parent but not children

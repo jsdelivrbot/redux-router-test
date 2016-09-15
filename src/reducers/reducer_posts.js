b353b03cc3a4a65ccf59542ff33adc26083cbe8d
samnags <sam.nagourney@gmail.com>
2016-09-15 14:11:01 -0400

    thank you stephen grider

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -8,8 +8,8 @@ export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FETCH_POSTS:
     return { ...state, all: action.payload.data };
-  case FETCH_POSTS:
-    return { ...state, all: action.payload.data };
+  case FETCH_POST:
+    return { ...state, post: action.payload.data };
   default:
     return state;
   }
